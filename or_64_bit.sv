1caee59ba5de74d04b898c0598109f52826d16b0
Gerin George <geringeorge92@gmail.com>
2022-10-26 12:41:11 -0700

    added or and xor

---
+++ or_64_bit.sv
@@ -0,0 +1,44 @@
+`timescale 1ps/1ps
+// 64 bit OR
+// Takes in two 64 bit inputs A and B and does bitwise OR for a 64 bit
+// output
+module or_64_bit(A, B, out);
+
+	//64 bit inputs
+	input logic [63:0] A, B;
+	
+	//64 bit result output
+	output logic [63:0] out;
+
+	genvar i;
+
+	generate 
+	    
+	    for(i = 0; i < 64; i++) begin : theBigAnd
+            or(#50)(out[i], A[i], B[i]);
+        end	    
+	endgenerate
+	
+
+endmodule
+
+module and_64_bit_testbench();
+    
+    logic [63:0] A, B, out;
+    
+    or_64_bit dut(A, B, out);
+    
+    initial begin
+        A = 64'b1; B = 64'b0; #60;
+        
+        A = 64'b1; B = 64'b1; #60;
+        
+        A = 64'b0; B = 64'b1; #60;
+        
+        A = 64'b0; B = 64'b0; #60;
+        
+        A= $random(); B = $random(); #60
+        
+    end
+
+endmodule
