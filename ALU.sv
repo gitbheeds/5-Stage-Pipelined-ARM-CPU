a3996291800402c0b66ae6e37aa6cd0e9d6ca33e
spbhi <shaun.bhide@gmail.com>
2022-10-27 14:42:14 -0700

    updated the 8x1 selector

--- ALU.sv
+++ ALU.sv
@@ -1,12 +1,22 @@
 `timescale 1ps/1ps
 
+//Simple ALU design
+// Flags:
+// negative: whether the result output is negative if interpreted as 2's comp.
+// zero: whether the result output was a 64-bit zero.
+// overflow: on an add or subtract, whether the computation overflowed if the inputs are interpreted as 2's comp.
+// carry_out: on an add or subtract, whether the computation produced a carry-out.
 
-//ALU testing 
-//supports pass, add, and subtract as of 10/26
-
-//TODO: AND, OR, XOR op support
-//rigorous testing
+// cntrl			Operation						Notes:
+// 000:			result = B						value of overflow and carry_out unimportant
+// 010:			result = A + B
+// 011:			result = A - B
+// 100:			result = bitwise A & B		value of overflow and carry_out unimportant
+// 101:			result = bitwise A | B		value of overflow and carry_out unimportant
+// 110:			result = bitwise A XOR B	value of overflow and carry_out unimportant
 module ALU (A, B, cntrl, result, overflow, negative, zero, carry_out);
+
+//define inputs and outputs
 
 	//64 bit inputs
 	input logic [63:0] A, B;
@@ -20,10 +30,9 @@ module ALU (A, B, cntrl, result, overflow, negative, zero, carry_out);
 	//output flags for ALU op results
 	output logic overflow, negative, zero, carry_out;
 	
-	//holds all the things to shove into ALU control mux
-	logic [7:0][63:0] ALU_inputs;
 	
 	
+//define wires for interconnects	
 	
 	//wire to determine whether to send B or ~B to the ALU
 	wire [63:0] B_adj;
@@ -34,37 +43,22 @@ module ALU (A, B, cntrl, result, overflow, negative, zero, carry_out);
 	//wires to send output of bitwise AND, OR, XOR to ALU op selector
 	wire [63:0] ANDtoALU, ORtoALU, XORtoALU;
 	
-		//populate ALU_inputs
+
 	
-	//cntrl 001 not supported
-	assign ALU_inputs [1] = 64'bX;
-	
-	//pass output of adder to both add and subtract lines of ALU op selector
-	assign ALU_inputs [2] = FAtoALU; //cntrl = 010
-	assign ALU_inputs [3] = FAtoALU; //cntrl = 011
-	
-	//pass output of bitwise AND to ALU op selector
-	assign ALU_inputs [4] = ANDtoALU; //cntrl = 100
-	
-	//pass output of bitwise OR to ALU op selector
-	assign ALU_inputs [5] = ORtoALU; //cntrl = 101
-	
-	//pass output of bitwise XOR to ALU op selector
-	assign ALU_inputs [6] = XORtoALU; //cntrl = 110
-	
-	//cntrl = 111 not supported
-	assign ALU_inputs [7] = 64'bX;
-	
-	
-	
+//adder + subtractor	
+
 	//set B into two's comp stage 1 if needed (invert bits)
-	mux64x2_1 twos(.sel(cntrl[0]), .i0(B), .i1(B), .out(B_adj));
+	mux64x2_1_2scomp twos(.sel(cntrl[0]), .i0(B), .i1(B), .out(B_adj));
 	
-	//adder
+
 	//when sub control = 1, sets B into two's comp stage 2 (add 1)
 	//sets overflow and carry_out flags
 	adder64_bit add_sub(.input1(A), .input2(B_adj), .sub_control(cntrl[0]), .out(FAtoALU), .of_flag(overflow), .co_flag(carry_out));
 	
+	
+	
+//logic operations
+
 	//bitwise AND
 	and_64_bit AND(.A, .B, .out(ANDtoALU));
 	
@@ -74,29 +68,22 @@ module ALU (A, B, cntrl, result, overflow, negative, zero, carry_out);
 	//bitwise XOR
 	xor_64_bit XOR(.A, .B, .out(XORtoALU));
 	
-	//set negative flag, 3 input AND 
-	//only if interpreted as two's comp. Two's comp only used in sub ops
-	//sub op: 011
-	//twos comp used -> MSB of result is sign
-	//negative = result[63] & cntrl[1] & cntrl[0]
-	and #(50) neg_flag(negative, result[63], cntrl[1], cntrl[0]);
 	
 	
+//set negative, zero flags
+	
+	//set negative flag
+	//negative = result[63]
+	assign negative = result[63];
 	
 	//set zero flag
 	zeroCheck flag(.check(result), .z_flag(zero));
 	
 	
 	
-	//pass B to ALU op selector for pass line
-	assign ALU_inputs [0] = B; //cntrl = 000
-	
-	
-
-	
-	
 	//ALU op selector
-	mux64x8_1 ALU_opSel(.in(ALU_inputs), .out(result), .ctrl(cntrl));
+	mux64x8_1 ALU_opSel(.in0(B), .in1(64'bX), .in2(FAtoALU), .in3(FAtoALU), .in4(ANDtoALU),  
+							.in5(ORtoALU), .in6(XORtoALU), .in7(64'bX), .out(result), .sel(cntrl));
 
 endmodule
 
@@ -130,8 +117,23 @@ module ALUmini ();
 		A = 0; B = 124;
 		#(delay); //R = -124, negative = 1, all other flags 0
 		
-		A = 64'b1; B = 1;
-		cntrl = ALU_ADD; #(delay); //R = xx, overflow = 1, carry_out = 1, all other flags 0
+		A = 64'hFFFFFFFFFFFFFFFF; B = 1;
+		cntrl = ALU_ADD; #(delay); //R = 16'hFFFFFFFFFFFFFFFF, overflow = 1, carry_out = 1, all other flags 0
+		
+		A = 0; B = 0; 
+		cntrl = ALU_AND; #(delay); //R = 0, zero = 0, all other flags X
+		
+		A = 64'hFFFFFFFFFFFFFFFF; B = 64'hFFFFFFFFFFFFFFF0;
+		#(delay); //R = 16'hFFFFFFFFFFFFFFF0, zero = 0, all other flags X
+		
+		cntrl = ALU_XOR; #(delay); //R = 16'h0000000000000001, zero = 0, all other flags X
+		
+		cntrl = ALU_OR; #(delay); //R = 16'hFFFFFFFFFFFFFFFF, zero = 0, all other flags X
+		
+		cntrl = 3'b001; #(delay); //all X
+		
+		cntrl = 3'b111; #(delay); //all X
+		
 	end
 	
 	
