3396ab962bf1661405227fde85eff44ac024a3fc
Gerin George <geringeorge92@gmail.com>
2022-10-26 21:33:16 -0700

    Alu testbench

--- ALU.sv
+++ ALU.sv
@@ -34,37 +34,7 @@ module ALU (A, B, cntrl, result, overflow, negative, zero, carry_out);
 	//wires to send output of bitwise AND, OR, XOR to ALU op selector
 	wire [63:0] ANDtoALU, ORtoALU, XORtoALU;
 	
-	
-	
-	//set B into two's comp stage 1 if needed (invert bits)
-	mux64x2_1 twos(.sel(cntrl[0]), .i0(B), .i1(B), .out(B_adj));
-	
-	//adder
-	//when sub control = 1, sets B into two's comp stage 2 (add 1)
-	//sets overflow and carry_out flags
-	adder64_bit add_sub(.input1(A), .input2(B_adj), .sub_control(cntrl[0]), .out(FAtoALU), .of_flag(overflow), .co_flag(carry_out));
-	
-	
-	
-	//set negative flag, 3 input AND 
-	//only if interpreted as two's comp. Two's comp only used in sub ops
-	//sub op: 011
-	//twos comp used -> MSB of result is sign
-	//negative = result[63] & cntrl[1] & cntrl[0]
-	and #(50) neg_flag(negative, result[63], cntrl[1], cntrl[0]);
-	
-	
-	
-	//set zero flag
-	zeroCheck flag(.check(result), .z_flag(zero));
-	
-	
-	
-	//pass B to ALU op selector for pass line
-	assign ALU_inputs [0] = B; //cntrl = 000
-	
-	
-	//populate ALU_inputs
+		//populate ALU_inputs
 	
 	//cntrl 001 not supported
 	assign ALU_inputs [1] = 64'bX;
@@ -86,7 +56,86 @@ module ALU (A, B, cntrl, result, overflow, negative, zero, carry_out);
 	assign ALU_inputs [7] = 64'bX;
 	
 	
+	
+	//set B into two's comp stage 1 if needed (invert bits)
+	mux64x2_1 twos(.sel(cntrl[0]), .i0(B), .i1(B), .out(B_adj));
+	
+	//adder
+	//when sub control = 1, sets B into two's comp stage 2 (add 1)
+	//sets overflow and carry_out flags
+	adder64_bit add_sub(.input1(A), .input2(B_adj), .sub_control(cntrl[0]), .out(FAtoALU), .of_flag(overflow), .co_flag(carry_out));
+	
+	//bitwise AND
+	and_64_bit AND(.A, .B, .out(ANDtoALU));
+	
+	//bitwise OR
+	or_64_bit OR(.A, .B, .out(ORtoALU));
+	
+	//bitwise XOR
+	xor_64_bit XOR(.A, .B, .out(XORtoALU));
+	
+	//set negative flag, 3 input AND 
+	//only if interpreted as two's comp. Two's comp only used in sub ops
+	//sub op: 011
+	//twos comp used -> MSB of result is sign
+	//negative = result[63] & cntrl[1] & cntrl[0]
+	and #(50) neg_flag(negative, result[63], cntrl[1], cntrl[0]);
+	
+	
+	
+	//set zero flag
+	zeroCheck flag(.check(result), .z_flag(zero));
+	
+	
+	
+	//pass B to ALU op selector for pass line
+	assign ALU_inputs [0] = B; //cntrl = 000
+	
+	
+
+	
+	
 	//ALU op selector
 	mux64x8_1 ALU_opSel(.in(ALU_inputs), .out(result), .ctrl(cntrl));
 
 endmodule
+
+module ALUmini ();
+
+	parameter delay = 100000;
+
+	logic		[63:0]	A, B;
+	logic		[2:0]		cntrl;
+	logic		[63:0]	result;
+	logic					negative, zero, overflow, carry_out ;
+
+	parameter ALU_PASS_B=3'b000, ALU_ADD=3'b010, ALU_SUBTRACT=3'b011, ALU_AND=3'b100, ALU_OR=3'b101, ALU_XOR=3'b110;
+	
+
+	ALU dut (.A, .B, .cntrl, .result, .negative, .zero, .overflow, .carry_out);
+	
+	initial begin
+	
+		cntrl = ALU_PASS_B;
+		A = 7000; B = 1888; #(delay);
+		
+		cntrl = ALU_ADD; #(delay); //R = 8888, zero = 0; carry_out = 0; overflow = 0; negative = 0;
+
+		B = 6999; 
+		cntrl = ALU_SUBTRACT; #(delay); //R = 1, all flags 0
+		
+		A = 6999;
+		#(delay); //R = 0, zero = 1, all other flags 0
+		
+		A = 0; B = 124;
+		#(delay); //R = -124, negative = 1, all other flags 0
+		
+		A = 64'b1; B = 1;
+		cntrl = ALU_ADD; #(delay); //R = xx, overflow = 1, carry_out = 1, all other flags 0
+	end
+	
+	
+	
+
+	
+endmodule 
---
+++ alurun.do
@@ -0,0 +1,39 @@
+# Create work library
+vlib work
+
+# Compile Verilog
+#     All Verilog files that are part of this design should have
+#     their own "vlog" line below.
+
+vlog "./ALU.sv"
+vlog "./mux2_1.sv"
+vlog "./mux4_1.sv"
+vlog "./mux8_1.sv"
+vlog "./mux64x8_1.sv"
+vlog "./mux64x2_1.sv"
+vlog "./fullAdder.sv"
+vlog "./adder64_bit.sv"
+vlog "./zeroCheck.sv"
+vlog "./and_64_bit.sv"
+vlog "./or_64_bit.sv"
+vlog "./xor_64_bit.sv"
+
+# Call vsim to invoke simulator
+#     Make sure the last item on the line is the name of the
+#     testbench module you want to execute.
+vsim -voptargs="+acc" -t 1ps -lib work ALUmini
+
+# Source the wave do file
+#     This should be the file that sets up the signal window for
+#     the module you are testing.
+do alu_wave.do
+
+# Set the window types
+view wave
+view structure
+view signals
+
+# Run the simulation
+run -all
+
+# End
